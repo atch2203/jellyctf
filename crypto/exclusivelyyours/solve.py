f14f411c799a938a4f8208edcfe34e3207ee02a2
Alexander Tong <55810435+atch2203@users.noreply.github.com>
2024-06-25 00:48:34 -0400

    writeups done

--- crypto/exclusivelyyours/solve.py
+++ crypto/exclusivelyyours/solve.py
@@ -1,6 +1,8 @@
 c = "06 1C 2F 38 3F 38 2C 29 09 0A 16 2D 1C 16 2B 31 17 1B 2D 0A 16 0F 18 1C 11"
 c = c.split()
 c = [int(i, 16) for i in c]
+
+print([chr(c[i]^ord("jellyCTF"[i])) for i in range(8)])
 res = ""
 key = "jel"
 for i in range(len(c)):
--- forensics/oshimark/solve.py
+++ forensics/oshimark/solve.py
@@ -1,13 +1,36 @@
+#with open('desc.txt', 'r') as f:
+#    bs = f.read()
+
+#bs.replace('\u200C', '')
+#print(bs.encode())
+
 a = ""
 with open("hex.txt", "r") as f:
     a = f.read()
-    a = a.split()
+a = a.split()
+b = a[2::7]
+a = a[3::7]
+a = [int(a[i],16)-(64+47 if b[i] == '84' else 47) for i in range(len(a))]
+
+print("".join([chr(x) for x in a]))
+#print(a)
+#a = a.split()
+#print(a)
+#print(bytes.fromhex("".join(a)).decode())
+""""
+a = a.split()
+a = a[4:-5]
+a = [a[i:i+7] for i in range(0, len(a), 7)]
+a = [int("".join(i), 16) for i in a]
+a = [(i+81)%192 for i in a]
+print(a)
+print(''.join([chr(i) for i in a]))
 
 u = "f3 a0 85 8e e2 80 8c".split()
 u = [int(x, 16) for x in u]
 s = len(u)
 letters = []
-negs = []
+diffs = []
 
 chunks = int(len(a)/s)
 for i in range(chunks):
@@ -15,17 +38,8 @@ for i in range(chunks):
     b = [int(x, 16) for x in b]
     diff = [b[i]-u[i] for i in range(s)]
     letters.append(chr(diff[3]+97))
-    negs.append(diff[2])
-    #print(diff)
+    diffs.append(diff[2])
 
-#print(letters)
-#print(negs)
-
-# print("".join(letters))
-
-for i in range(chunks):
-    # if negs[i] == -1:
-    if letters[i] == "p":
-        print(f"{letters[i]} {negs[i]}")
-    
-
+print(letters)
+print(diffs)
+"""
